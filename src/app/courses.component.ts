80367ca850d94a573019b2b50546a050c23929fc
danielwood <dwooddev@gmail.com>
2020-01-08 16:30:15 -0500

    Up to lesson 45 in class

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,36 @@
 import {Component} from '@angular/core';
+import {CoursesService} from './courses.service';
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: `
+    <input #email (keyup.enter)="onKeyUp(email.value)">
+    <h2>{{"Title: " + title}}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+          {{ course }}
+        </li>
+    </ul>
+    <div (click)="onDivClicked()">
+    <button (click) = "onSave($event)" class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'"[class.active]="isActive">Oh Yeah!</button>
+    </div>
+  `
 })
 export class CoursesComponent {
-
+  title = "List of courses";
+  courses;
+  isActive = true;
+  onDivClicked(){
+    console.log('Div was clicked!');
+  }
+  onSave($event){
+    $event.stopPropagation();
+    console.log('Rooday! Rooday!', $event);
+  }
+  onKeyUp(email){
+    console.log('Email! ' + email);
+  }
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 }
